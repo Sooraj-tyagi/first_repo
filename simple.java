c43468114bf85bd9e8b7eb5b4567800b0eef4cae
Sooraj_tyagi <soorajtyagi8sept99@gmail.com>
2021-04-12 23:30:02 +0530

    first change

---
+++ simple.java
@@ -0,0 +1,18 @@
+package world;
+
+public class simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Thread t=new Thread();
+        String s=new String();
+        Object o=new Object();
+	
+		//System.out.println(t==o);
+		//System.out.println('a'='b');
+		//System.out.println(s==o);
+		System.out.println(t==s);
+		
+	}
+
+}
